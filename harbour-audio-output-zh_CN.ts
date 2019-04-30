c3fb6465b236ee7e727b608127e4f1f98b0e6280
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-30 20:51:33 +0800

    Create harbour-audio-output-zh_CN.ts

---
+++ harbour-audio-output-zh_CN.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="13"/>
+        <source>Audio output</source>
+        <extracomment>Header of the cover</extracomment>
+        <translation>音频输出</translation>
+    </message>
+</context>
+<context>
+    <name>SelectPage</name>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="37"/>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="43"/>
+        <source>Select audio output</source>
+        <translation>选择音频输出</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="72"/>
+        <source>Available</source>
+        <extracomment>Available refers to a certain port being available Unavailable refers to a cetrain port not being available</extracomment>
+        <translation>可用</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="72"/>
+        <source>Not available</source>
+        <translation>不可用</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="74"/>
+        <source>Priority: %1</source>
+        <extracomment>Priority has to do something with the preference of port</extracomment>
+        <translation>优先考虑: %1 </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectPage.qml" line="90"/>
+        <source>Couldn&apos;t find any audio outputs</source>
+        <extracomment>Shown when no ports/sinks could be found</extracomment>
+        <translation>无法找到任何音频输出</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-audio-output</name>
+    <message>
+        <location filename="../qml/harbour-audio-output.qml" line="18"/>
+        <source>Error: %s</source>
+        <translation>错误: %s</translation>
+    </message>
+</context>
+</TS>
